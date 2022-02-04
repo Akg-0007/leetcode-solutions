6a60cb1e4c5cc6cb0f491d9e85de05f8c8b0429b
Akg-0007 <63384864+Akg-0007@users.noreply.github.com>
2022-02-04 12:41:25 +0530

    Time: 3 ms (53.84%), Space: 6 MB (47.27%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return __builtin_popcount(n);
+       // return __builtin_popcount(n);
+        int c=0;
+        while(n){
+            if(n&1==1)c++;
+            n>>=1;
+        }    
+        
+        return c;
     }
 };
