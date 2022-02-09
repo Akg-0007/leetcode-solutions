7ed53f579b6f44f50cc22fd10d829b07407cb53a
Akg-0007 <63384864+Akg-0007@users.noreply.github.com>
2022-02-09 21:09:12 +0530

    Added solution - LeetHub

--- Ways To Tile A Floor - GFG/ways-to-tile-a-floor.cpp
+++ Ways To Tile A Floor - GFG/ways-to-tile-a-floor.cpp
@@ -5,14 +5,19 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
+  
+    long long shiv(long long n,long long dp[]){
+        if(n==1 || n==0)return dp[n]=1;
+      if(dp[n]!=-1){return dp[n];}
+      
+     dp[n]=(shiv(n-1,dp)+shiv(n-2,dp))%1000000007;
+     return dp[n];
+    }
     long long numberOfWays(long long n) {
         //code here
         long long dp[n+1];
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<n+1;i++){
-            dp[i]=(dp[i-1]+dp[i-2])%1000000007;
-        }
+         memset(dp,-1,sizeof dp );
+        shiv(n,dp);
         return dp[n]; 
     }
 };
