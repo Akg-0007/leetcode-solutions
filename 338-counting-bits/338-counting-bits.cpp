9d6265e773a8f236dc9aeaffe5b075b611949880
Akash Gupta <63384864+Akg-0007@users.noreply.github.com>
2022-03-01 08:57:30 +0530

    Time: 4 ms (88.16%), Space: 8 MB (48.88%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>v(n+1);
+            v[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+           v[i]= __builtin_popcount(i);
+        }
+        return v;
+    }
+};
