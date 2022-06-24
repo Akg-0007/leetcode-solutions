f0c420b024e08f17481cd5da38264fd770d589cd
Akash Gupta <63384864+Akg-0007@users.noreply.github.com>
2022-06-24 11:47:23 +0530

    Time: 59 ms (45.14%), Space: 19.1 MB (46.05%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& v) {
-        stack<pair<int,int>>s;
-        sort(v.begin(),v.end());
-        s.push({v[0][0],v[0][1]});
-        for(int i=1;i<v.size();i++)
-        {
-           int l = v[i][0], r = v[i][1];
-            pair<int,int> top = s.top();
-            if(top.second>=l){s.pop();
-            s.push({top.first,max(top.second,r)});}
-            else{ s.push({ l, r });}
+        sort(v.begin(), v.end());
+        vector<vector<int>> o;
+        o.push_back(v[0]);
+        for(int i=1; i<v.size(); i++) {
+            if(o.back()[1] >= v[i][0]) o.back()[1] = max(o.back()[1] , v[i][1]);
+            else o.push_back(v[i]); 
         }
-        vector<vector<int>>re;
-        while(!s.empty())
-        {
-            re.push_back({s.top().first,s.top().second});
-            s.pop();
-        }
-        return re;
+        return o; 
     }
 };
