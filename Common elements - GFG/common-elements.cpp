63207917ddfa63963f01259d57cb8d6b8e8701a0
Akash Gupta <63384864+Akg-0007@users.noreply.github.com>
2022-06-27 15:35:49 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.cpp
@@ -0,0 +1,66 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            //code her
+            unordered_map<int,int>m;
+            int c=0;
+            vector<int>v;
+            for(int i=0;i<n1;i++)
+            {
+                if(A[i]!=A[i+1])m[A[i]]++;
+            }
+            for(int i=0;i<n2;i++)
+            {
+                if(B[i]!=B[i+1])m[B[i]]++;
+            }
+            for(int i=0;i<n3;i++)
+            {
+                if(C[i]!=C[i+1])m[C[i]]++;
+            }
+            for(auto i:m)
+            {if(i.second==3){v.push_back(i.first);}
+                
+            }
+             sort(v.begin(),v.end());
+             return v;
+             
+            
+        }
+
+};
+
+// { Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
+    }
+}  // } Driver Code Ends
